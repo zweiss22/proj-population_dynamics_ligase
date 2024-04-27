8be3181b6d38ce4a867e9d0262239948cbe168f9
Zoe <weiss.zoe@gmail.com>
2024-04-27 13:27:33 -0400

    updates

--- scripts/020_enrichment/plot_family_enrichment.py
+++ scripts/020_enrichment/plot_family_enrichment.py
@@ -9,7 +9,7 @@ final_clusters = pd.read_csv(clustered_path + 'clusters_round'+str(8)+'.csv')
 
 all_peak_counts = []
 
-for cluster in range(1,9):
+for cluster in range(1,11):
     print(cluster)
     peak = list(final_clusters[final_clusters.Ranked_Cluster == cluster].Sequence)[0]
     peak_counts_list = []
@@ -21,19 +21,3 @@ for cluster in range(1,9):
     all_peak_counts.append(peak_counts_list)
 
 pd.DataFrame(all_peak_counts).to_csv('/Users/zoeweiss/Desktop/evolution_manuscript/proj-population_dynamics_ligase/data/peak_enrichment_fraction.csv')
-
-# Plotting each row as a line
-plt.figure(figsize=(10, 6))
-
-for index, row in pd.DataFrame(all_peak_counts).iterrows():
-    plt.plot(row, label=f'Family {1+index}')
-
-# Adding labels and title
-plt.xlabel('Round', fontsize=18)
-plt.ylabel('Fraction abundance', fontsize=18)
-plt.xticks(np.arange(8), np.arange(1, 9), fontsize=18)
-
-plt.yticks(fontsize=18)
-plt.legend(fontsize=18)
-
-plt.savefig('/Users/zoeweiss/Desktop/evolution_manuscript/proj-population_dynamics_ligase/data/family_enrichment.png')
